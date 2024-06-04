f8ba2791ed30ae18d45c0f666c7e06cf3a09b3bc
Yevgeniy5056 <yevgengeodakyan@gmail.com>
2024-06-03 23:50:45 -0400

    data.sql updated, SecurityConfig updated

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,17 +17,44 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(new User("mike", encoder.encode("password"),
+//                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//        users.add(new User("ozzy", encoder.encode("password"),
+//                List.of(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//
+//        return new InMemoryUserDetailsManager(users);
+//
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        List<UserDetails> users = new ArrayList<>();
-        users.add(new User("mike", encoder.encode("password"),
-                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-        users.add(new User("ozzy", encoder.encode("password"),
-                List.of(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-
-        return new InMemoryUserDetailsManager(users);
-
+        return http
+                .authorizeRequests()
+                .antMatchers("/user/**").hasAuthority("ROLE_ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragmets/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/welcome")
+                    .failureUrl("/login?error=true")
+                    .permitAll()
+                .and().build();
     }
 
 }
