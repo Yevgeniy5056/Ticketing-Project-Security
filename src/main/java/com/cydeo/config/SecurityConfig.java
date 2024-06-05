1baab93d86f3fb10937b4ed64fa3a9c01230512c
Yevgeniy5056 <yevgengeodakyan@gmail.com>
2024-06-05 10:48:32 -0400

    Logout, remember me added

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -1,22 +1,20 @@
 package com.cydeo.config;
 
+import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
+    private final SecurityService securityService;
+
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 //    @Bean
 //    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 //
@@ -35,10 +33,10 @@ public class SecurityConfig {
 
         return http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasAuthority("Admin")
-                .antMatchers("/project/**").hasAuthority("Manager")
-                .antMatchers("/task/employee/**").hasAuthority("Employee")
-                .antMatchers("/task/**").hasAuthority("Manager")
+                    .antMatchers("/user/**").hasAuthority("Admin")
+                    .antMatchers("/project/**").hasAuthority("Manager")
+                    .antMatchers("/task/employee/**").hasAuthority("Employee")
+                    .antMatchers("/task/**").hasAuthority("Manager")
 //                .antMatchers("/task/**").hasAnyAuthority("ROLE_EMPLOYEE", "ROLE_ADMIN")
 //                .antMatchers("/task/**").hasAnyRole("EMPLOYEE", "ADMIN")
                 .antMatchers(
@@ -54,8 +52,18 @@ public class SecurityConfig {
                     .loginPage("/login")
                     .defaultSuccessUrl("/welcome")
                     .failureUrl("/login?error=true")
-                    .permitAll()
-                .and().build();
+                .permitAll()
+                .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds(120)
+                    .key("key")
+                    .userDetailsService(this.securityService)
+                .and()
+                .build();
     }
 
 }
